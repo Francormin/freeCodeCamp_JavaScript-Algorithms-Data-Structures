e27c838b40234b15a46716a7cf28e755cef5378b
Franco <francocorniglione5@gmail.com>
2023-09-14 19:05:35 -0300

    editing algorithm description

--- algorithm-scripting/basic/06/index.js
+++ algorithm-scripting/basic/06/index.js
@@ -1,8 +1,13 @@
-// Confirm the Ending
+/*
+       ____________________
+----- | Confirm the Ending | -----
 
-// Check if a string (first argument, str) ends with the given target string (second argument, target).
+Check if a string (first argument, str) ends with the given target string (second argument, target).
 
-// This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose
+of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+*/
 
 function confirmEnding(str, target) {
   return str.slice(str.length - target.length) === target;
