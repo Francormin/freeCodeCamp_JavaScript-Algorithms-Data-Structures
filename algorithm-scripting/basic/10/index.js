eff233275bc455b20c9bd830ba7e6c4b75b53620
Franco <francocorniglione5@gmail.com>
2023-09-14 19:07:11 -0300

    editing algorithm description

--- algorithm-scripting/basic/10/index.js
+++ algorithm-scripting/basic/10/index.js
@@ -1,8 +1,12 @@
-// Boo who
+/*
+       _________
+----- | Boo who | -----
 
-// Check if a value is classified as a boolean primitive. Return true or false.
+Check if a value is classified as a boolean primitive. Return true or false.
 
-// Boolean primitives are true and false.
+Boolean primitives are true and false.
+
+*/
 
 function booWho(bool) {
   return typeof bool === "boolean";
