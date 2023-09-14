f562fed8f3fbcc3be9925e75e97e453273762fe1
Franco <francocorniglione5@gmail.com>
2023-09-14 19:07:40 -0300

    editing algorithm description

--- algorithm-scripting/basic/12/index.js
+++ algorithm-scripting/basic/12/index.js
@@ -1,12 +1,16 @@
-// Slice and Splice
+/*
+       __________________
+----- | Slice and Splice | -----
 
-// You are given two arrays and an index.
+You are given two arrays and an index.
 
-// Copy each element of the first array into the second array, in order.
+Copy each element of the first array into the second array, in order.
 
-// Begin inserting elements at index n of the second array.
+Begin inserting elements at index n of the second array.
 
-// Return the resulting array. The input arrays should remain the same after the function runs.
+Return the resulting array. The input arrays should remain the same after the function runs.
+
+*/
 
 function frankenSplice(arr1, arr2, n) {
   const localArr = arr2.slice();
