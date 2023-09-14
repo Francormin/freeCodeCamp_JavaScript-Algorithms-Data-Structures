9cc04f7f5b46fcadcfa65d58da9ecb94831e3831
Franco <francocorniglione5@gmail.com>
2023-09-14 19:07:28 -0300

    editing algorithm description

--- algorithm-scripting/basic/11/index.js
+++ algorithm-scripting/basic/11/index.js
@@ -1,8 +1,12 @@
-// Title Case a Sentence
+/*
+       _______________________
+----- | Title Case a Sentence | -----
 
-// Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
 
-// For the purpose of this exercise, you should also capitalize connecting words like the and of.
+For the purpose of this exercise, you should also capitalize connecting words like the and of.
+
+*/
 
 function titleCase(str) {
   return str
