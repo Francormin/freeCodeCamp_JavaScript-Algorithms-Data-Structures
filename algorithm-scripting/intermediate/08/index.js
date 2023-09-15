01c3ba0ce0eb1db43447568295af082682bd606a
Franco <francocorniglione5@gmail.com>
2023-09-15 13:22:04 -0300

    algorithm description stuff

--- algorithm-scripting/intermediate/08/index.js
+++ algorithm-scripting/intermediate/08/index.js
@@ -8,7 +8,7 @@ building blocks of the DNA double helix.
 The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA
 strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
 
-For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]].
+For example, for the input GCG, return [["G", "C"], ["C", "G"], ["G", "C"]].
 
 The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 
