357b76414270b38fa565a90979cfb0026faf2c34
Franco <francocorniglione5@gmail.com>
2023-09-15 12:03:33 -0300

    editing algorithm description

--- algorithm-scripting/basic/13/index.js
+++ algorithm-scripting/basic/13/index.js
@@ -1,10 +1,14 @@
-// Falsy Bouncer
+/*
+       _______________
+----- | Falsy Bouncer | -----
 
-// Remove all falsy values from an array. Return a new array; do not mutate the original array.
+Remove all falsy values from an array. Return a new array; do not mutate the original array.
 
-// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
 
-// Hint: Try converting each value to a Boolean.
+Hint: Try converting each value to a Boolean.
+
+*/
 
 function bouncer(arr) {
   return arr.filter(element => Boolean(element));
