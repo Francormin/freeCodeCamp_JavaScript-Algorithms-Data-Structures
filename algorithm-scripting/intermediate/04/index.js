7d96398bc866a35329d3f1f6180f10a614d702c4
Franco <francocorniglione5@gmail.com>
2023-09-15 13:09:52 -0300

    algorithm description stuff

--- algorithm-scripting/intermediate/04/index.js
+++ algorithm-scripting/intermediate/04/index.js
@@ -1,6 +1,6 @@
 /*
-
-Wherefore art thou
+       ____________________
+----- | Wherefore art thou | -----
 
 Make a function that looks through an array of objects (first argument) and returns an array of all objects that
 have matching name and value pairs (second argument). Each name and value pair of the source object has to be
