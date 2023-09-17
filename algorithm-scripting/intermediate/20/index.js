92ab2d9f51e1879ea883f3d87a2abea7d32ec221
Franco <francocorniglione5@gmail.com>
2023-09-17 15:40:06 -0300

    editing algorithm descriptions

--- algorithm-scripting/intermediate/20/index.js
+++ algorithm-scripting/intermediate/20/index.js
@@ -1,15 +1,20 @@
-// Make a Person
+/*
+       _______________
+----- | Make a Person | -----
 
-// Fill in the object constructor with the following methods below:
+Fill in the object constructor with the following methods below:
 
-// getFirstName()
-// getLastName()
-// getFullName()
-// setFirstName(first)
-// setLastName(last)
-// setFullName(first, last)
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(first, last)
 
-// Run the tests to see the expected output for each method. These methods must be the only available means of interacting with the object. Each test will declare a new Person instance as new Person('Bob', 'Ross').
+Run the tests to see the expected output for each method. These methods must be the only available means
+of interacting with the object. Each test will declare a new Person instance as new Person("Bob", "Ross").
+
+*/
 
 const Person = function (first, last) {
   let firstName = first;
