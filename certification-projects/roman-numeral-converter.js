970985c59f004cffc0f6b03fa9484a46c9434928
Franco <francocorniglione5@gmail.com>
2023-09-20 09:48:23 -0300

    uploading new certification project

---
+++ certification-projects/roman-numeral-converter.js
@@ -0,0 +1,54 @@
+// Roman Numeral Converter
+
+// Convert the given number into a roman numeral.
+
+// Roman numerals  | 	 Arabic numerals
+//      M	         |        1000
+//      CM	       |        900
+//      D	         |        500
+//      CD	       |        400
+//      C          |        100
+//      XC	       |        90
+//      L	         |        50
+//      XL	       |        40
+//      X          |        10
+//      IX	       |        9
+//      V          |        5
+//      IV         |     	  4
+//      I	         |        1
+
+// All roman numerals answers should be provided in upper-case.
+
+function convertToRoman(num) {
+  const rules = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+
+  let result = "";
+  const romans = Object.keys(rules);
+
+  for (let i = 0; i < romans.length; ++i) {
+    const val = rules[romans[i]];
+
+    while (num >= val) {
+      num -= val;
+      result += romans[i];
+    }
+  }
+
+  return result;
+}
+
+convertToRoman(36);
