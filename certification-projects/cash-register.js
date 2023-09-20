f4851d7a4825524683f3dd44ab653ec6a590ef61
Franco Corniglione <89218100+Francormin@users.noreply.github.com>
2023-09-20 10:19:09 -0300

    Update cash-register.js

--- certification-projects/cash-register.js
+++ certification-projects/cash-register.js
@@ -22,11 +22,11 @@ highest to lowest order, as the value of the change key.
 |-------------------------|--------------------------|
 |          Penny          |      $0.01 (PENNY)       |
 |          Nickel         |      $0.05 (NICKEL)      |
-|           Dime	     |       $0.1 (DIME)        |
+|           Dime          |       $0.1 (DIME)        |
 |          Quarter        |     $0.25 (QUARTER)      |
-|          Dollar	     |        $1 (ONE)          |
+|          Dollar         |        $1 (ONE)          |
 |       Five Dollars      |        $5 (FIVE)         |
-|       Ten Dollars	     |        $10 (TEN)         |
+|       Ten Dollars       |        $10 (TEN)         |
 |      Twenty Dollars     |       $20 (TWENTY)       |
 |   One-hundred Dollars   |    $100 (ONE HUNDRED)    |
 
